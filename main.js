967bc6066241179f75ea488799be34fb1aa559d2
grnthubbard <granthubbard02@gmail.com>
2025-02-10 16:31:31 -0500

    Incorporated more comments, making the code more legible. Also decided to fix the code to validate input and gain variables for the inputs.

--- main.js
+++ main.js
@@ -6,32 +6,42 @@ function Book(title, author, pages, readStatus) {
     this.pages = pages;
     this.readStatus = readStatus;
 }
-
+//Function to add a book to the library array
 function addBookToLibrary(title, author, pages, readStatus) {
   let newBook = new Book(title, author, pages, readStatus);
   myLibrary.push(newBook);
 }
-
+//Sample pushes
 addBookToLibrary("Cosmos", "Carl Sagan", "365", "No");
 addBookToLibrary("Looking For Alaska", "John Green", "221", "Yes");
-
+//Log the samples
 for (let item of myLibrary) {
     console.log(item);
 }
 
+//Add a listener so when I click on the buttom, it displays the form ("display:block");
 const element = document.getElementById("addBook");
 element.addEventListener("click", myFunction);
 function myFunction() {
     document.getElementById("bookForm").style.display = "block";
 }
 
-const title = document.getElementById("title");
+document.getElementById("bookForm").addEventListener("submit", function(event) {
+    event.preventDefault(); //Page will no longer refresh when I press submit.
 
-document.getElementById("submitBtn").addEventListener("submit", function(event){
-    if(title = ""){
-        
+    //Instead, we need to get the input values
+    const title = document.getElementById("title").value.trim();
+    const author = document.getElementById("author").value.trim();
+    const pages = document.getElementById("pages").value.trim();
+    const readStatus = document.getElementById("readStatus").value.trim();
+
+    //Make sure you validate the input (no blanks)
+    if (title === "" || author ==="" || pages === "") {
+        alert("Please fill out all the fields.");
+        return;
     }
-    event.preventDefault()
+
 });
+
 
 
